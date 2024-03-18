935aedf530607bdf2b8d5d751bf7e7b4f3e51172
ShirinAziz <Shirin.aziz96@outlook.com>
2024-03-18 11:53:55 +0100

    changed fro a tag to Link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import { FaFacebook, FaTwitter, FaInstagram, FaYoutube } from "react-icons/fa";
 import Button from "./Button";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -14,54 +15,54 @@ const Footer = () => {
         <div className="">
           <p>QUICK LINKS</p>
           <div className="flex flex-col text-start mb:4 md:mb-0">
-            <a
-              href="#"
+            <Link
+              to="/"
               className="block md:inline-block py-2 hover:inline-block"
             >
               Home
-            </a>
-            <a
-              href="#"
+            </Link>
+            <Link
+              to="/"
               className="block md:inline-block py-2 hover:inline-block"
             >
               About
-            </a>
-            <a
-              href="#"
+            </Link>
+            <Link
+              to="/"
               className="block md:inline-block py-2 hover:inline-block"
             >
               Contact
-            </a>
+            </Link>
           </div>
         </div>
         <div className="flex flex-col">
           <p>SOCIAL MEDIA</p>
           <div className="flex mt-4 gap-3">
-            <a
-              href="#"
+            <Link
+              to="/"
               className="bg-blue-600 p-1.5 rounded-sm text-white hover:text-gray-500 hover:scale-110"
             >
               <FaFacebook size={18} />
-            </a>
+            </Link>
 
-            <a
-              href="#"
+            <Link
+              to="/"
               className="bg-pink-600 p-1.5 rounded-sm text-white hover:text-gray-500 hover:scale-110"
             >
               <FaInstagram size={18} />
-            </a>
-            <a
-              href="#"
+            </Link>
+            <Link
+              to="/"
               className="bg-blue-600 p-1.5 rounded-sm text-white hover:text-gray-500 hover:scale-110"
             >
               <FaTwitter size={18} />
-            </a>
-            <a
-              href="#"
+            </Link>
+            <Link
+              to="/"
               className="bg-red-600 p-1.5 rounded-sm text-white hover:scale-110"
             >
               <FaYoutube size={18} />
-            </a>
+            </Link>
           </div>
 
           <Button
